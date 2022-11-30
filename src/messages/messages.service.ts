c87468ddd3e65c1ccb5d3fd325658f7df3c21e3c
Rahul Kumar <rahul.kumar@brainvire.com>
2022-11-30 16:02:12 +0530

    commit :: created  Services

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,21 @@
+import { MessageRepository } from './messages.repository';
+
+export class MessageServices {
+  messagesRepo: MessageRepository;
+
+  constructor() {
+    this.messagesRepo = new MessageRepository();
+  }
+
+  findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  }
+
+  findAll() {
+    return this.messagesRepo.findAll();
+  }
+
+  create(content: string) {
+    return this.messagesRepo.create(content);
+  }
+}
